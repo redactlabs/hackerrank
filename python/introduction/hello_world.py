85c20e93f1c05ef1d2a01ce538470efab2117268
redact <redact@rl-dell01.cb.zz>
2019-07-03 21:07:58 -0700

    Add Python and update README.md

---
+++ python/introduction/hello_world.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Written by	: Brian Vianzon <rufio@redactlabs.info>
+# Script Name	: hello_world.py
+
+if __name__ == '__main__':
+    print("Hello, World!")
---
+++ python/introduction/if-else-weird.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Written by	: Brian Vianzon <rufio@redactlabs.info>
+# Script Name	: if-else-weird.py
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
